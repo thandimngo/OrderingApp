317f6853e4fbd2c29eef954ecfbd7ec60463b7d3
thandimngo <thandimngomezulu0@gmail.com>
2016-09-14 15:26:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/orderingapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.admin.orderingapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int quantity = 2;
+
+    public void submitOrder(View view) {
+        int price = quantity * 5;
+        String priceMessage = "Total:R" + price;
+        priceMessage = priceMessage + "\nThank you";
+        displayMessage(priceMessage);
+
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+
+
+    }
+}
